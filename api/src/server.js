fe1d80191ae78389cba15aec7967731c2ef36c28
Margarita <margarita.petrova.kn@gmail.com>
2021-11-01 13:40:15 +0200

    Use routes and json in server.js
    Import routes in server.js and use them.
    Use json middleware.

--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ text: 'It\'s working' });
 });
 
+app.use(routes);
 app.listen(3030, () => console.log('App is running on port 3030'));
