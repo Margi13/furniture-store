d781a96083cb8b9ed97093d9b2ba883c4e62586e
Margarita <margarita.petrova.kn@gmail.com>
2021-11-01 16:39:31 +0200

    Update authmiddleware
    Fix header name for authorization with lower case.
    Export isAuth function to find if user is logged in.
    Fix json response when there is error in functions

--- api/src/middlewares/authMiddleware.js
+++ api/src/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../constants');
 
 exports.auth = function(req, res, next) {
-    let token = req.headers['X-Authorization'];
+    let token = req.headers['x-authorization'];
 
     if (token) {
         let decodedToken = jwt.verify(token, JWT_SECRET);
@@ -11,9 +11,16 @@ exports.auth = function(req, res, next) {
 
             next();
         } else {
-            res.status(401).json('You are not authorized');
+            res.status(401).json({ message: 'You are not authorized' });
         }
     } else {
         next();
     }
 }
+exports.isAuth = function(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.status(401).json({ message: 'You are not authorized' });
+    }
+};
