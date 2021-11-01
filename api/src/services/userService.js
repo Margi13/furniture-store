2de8ef8557057e0270528eb35156602065f9c442
Margarita <margarita.petrova.kn@gmail.com>
2021-11-01 14:54:38 +0200

    Add login method in user service
    Find user, validate password and then create token for this user and return it.

--- api/src/services/userService.js
+++ api/src/services/userService.js
@@ -1,3 +1,30 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = require('../constants');
 
 exports.register = ({ email, password }) => User.create({ email, password });
+
+exports.login = async({ email, password }) => {
+    let user = await User.findOne({ email });
+
+    // If there is user
+    if (!user) {
+        throw new Error('Invalid username or password');
+    }
+
+    // If this is his password
+    let isValid = await user.validatePassword(password);
+    if (!isValid) {
+        throw new Error('Invalid username or password');
+    }
+
+    let payload = {
+        _id: user._id,
+        email: user.email
+    };
+
+    let token = await jwt.sign(payload, JWT_SECRET);
+
+    return token;
+}
