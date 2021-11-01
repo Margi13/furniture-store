44ee1289608fa96622bbb87935f711c7dd7ca687
Margarita <margarita.petrova.kn@gmail.com>
2021-11-01 16:21:43 +0200

    Add getOne function in furniture service

--- api/src/services/furnitureService.js
+++ api/src/services/furnitureService.js
@@ -1,5 +1,6 @@
 const Furniture = require('../models/Furniture');
 
 exports.getAll = () => Furniture.find();
+exports.getOne = (furnitureId) => Furniture.findById(furnitureId);
 
 exports.create = (furnitureData) => Furniture.create(furnitureData);
