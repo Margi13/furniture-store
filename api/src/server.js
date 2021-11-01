0f3d5e0ae9b542bf2776c41110a4667f24434092
Margarita <margarita.petrova.kn@gmail.com>
2021-11-01 14:10:17 +0200

    Add cors library
    Install cors library.
    Allow cors using cors library

--- api/src/server.js
+++ api/src/server.js
@@ -1,23 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-    // Can access requests from this origin
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Can make this kind of requests
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-
-    //Client cannot add other headers than above
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-authorization');
-
-    next();
-})
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({ text: 'It\'s working' });
