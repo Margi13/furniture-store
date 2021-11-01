293d21a3e2f2974bd14e2d1a2ec20a434781cc9c
Margarita <margarita.petrova.kn@gmail.com>
2021-11-01 16:40:53 +0200

    Update catalog controller
    Added isAuth function from middleware.
    Pass userId when creating furniture.

--- api/src/controllers/catalogController.js
+++ api/src/controllers/catalogController.js
@@ -1,18 +1,23 @@
 const router = require('express').Router();
+
 const furnitureService = require('../services/furnitureService');
+
+const { isAuth } = require('../middlewares/authMiddleware');
 
 router.get('/', async(req, res) => {
     let furniture = await furnitureService.getAll();
     res.json(furniture);
 });
-router.post('/', async(req, res) => {
-    await furnitureService.create(req.body);
+router.post('/', isAuth, async(req, res) => {
+    await furnitureService.create({...req.body, ownerId: req.user._id });
 
     res.json({ ok: true });
 });
+
 router.get('/:furnitureId', async(req, res) => {
     let furniture = await furnitureService.getOne(req.params.furnitureId);
 
     res.json(furniture);
 });
+
 module.exports = router;
