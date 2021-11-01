65892f435a4a584aef1eab13549f02337e005126
Margarita <margarita.petrova.kn@gmail.com>
2021-11-01 16:22:23 +0200

    Get details page for furniture
    Added get request for page with details for furniture

--- api/src/controllers/catalogController.js
+++ api/src/controllers/catalogController.js
@@ -9,6 +9,10 @@ router.post('/', async(req, res) => {
     await furnitureService.create(req.body);
 
     res.json({ ok: true });
-})
+});
+router.get('/:furnitureId', async(req, res) => {
+    let furniture = await furnitureService.getOne(req.params.furnitureId);
 
+    res.json(furniture);
+});
 module.exports = router;
