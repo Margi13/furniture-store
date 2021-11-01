9b3010127966ff2aac50b12b8d04cb302c2febf7
Margarita <margarita.petrova.kn@gmail.com>
2021-11-01 14:19:36 +0200

    Add Database
    Add MongoDB directly in server.js

--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/furniture')
+    .then(() => {
+        console.log('DB Connected');
+    })
+    .catch((error) => {
+        console.log('DB Error:', error);
+    });
 
 app.use(express.json());
 app.use(cors());
