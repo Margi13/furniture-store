6b907db6e71ba689139b40d38c8b3cad8dca3d32
Margarita <margarita.petrova.kn@gmail.com>
2021-11-01 13:37:21 +0200

    Create userController

---
+++ api/src/controllers/userController.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.post('/register', (req, res) => {
+
+    console.log(req.body);
+
+    res.json({ ok: true });
+})
+
+
+module.exports = router;
